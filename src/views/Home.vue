da727d2c60dee34af635b69183ecea8c55112b43
foxscallion <foxscallion@163.com>
2022-01-21 18:24:53 +0800

    添加头部导航栏

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,35 @@
   <div class="home">
     <!--    头部-->
     <div class="head">
-      头部
+      <div class="left">left</div>
+      <div class="right">
+        <a-menu v-model:selectedKeys="current" mode="horizontal">
+          <a-menu-item key="article">
+            文章列表
+          </a-menu-item>
+          <a-menu-item key="sort">
+            归档分类
+          </a-menu-item>
+          <a-menu-item key="my">
+            个人中心
+          </a-menu-item>
+        </a-menu>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import {message} from 'ant-design-vue';
-import {defineComponent} from 'vue';
+import {defineComponent, ref} from 'vue';
 import {getData} from '@/api'
 
 export default defineComponent({
   name: 'Home',
-  components: {},
+  components: {
+  },
   setup() {
+    const current = ref<string[]>(['mail']);
     const info = () => {
       message.info('This is a normal message');
       getData().then(function (res) {
@@ -25,6 +40,7 @@ export default defineComponent({
     };
     return {
       info,
+      current
     };
   },
 });
@@ -32,7 +48,10 @@ export default defineComponent({
 <style lang="less" scoped>
 .home {
   .head {
-    color: aquamarine;
+    display: flex;
+    justify-content: space-between;
+    background: #fff;
+    border-bottom: 1px solid #eee;
   }
 }
 </style>
